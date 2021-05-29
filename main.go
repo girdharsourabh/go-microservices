89f2bd297e6dc8af3a4d4a2f132883ea907d790f
girdharsourabh <girdharsourabh@gmail.com>
2021-05-29 15:04:58 +0530

    Added handler and data

--- main.go
+++ main.go
@@ -1,24 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/girdharsourabh/handlers"
 )
 
 func main() {
+	l := log.New(os.Stdout, "product-api", log.Flags())
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
+	sm := http.NewServeMux()
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
+
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+	sm.Handle("/products", handlers.NewProducts(l))
+	s := &http.Server{
+		Addr:         ":9000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
-		fmt.Fprintf(rw, "Hello %s", d)
+	}()
 
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.ListenAndServe(":9000", nil)
+	sig := <-sigChan
+	l.Println("received terminate , graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+
 }
